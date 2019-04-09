bd4e6ee2dcd03a0da34db7cbf34032728c530dca
Wulf2k <wulf2k@gmail.com>
2019-04-08 20:59:28 -0500

    Update Form1.vb

--- PS4-TwitchControl/Form1.vb
+++ PS4-TwitchControl/Form1.vb
@@ -1180,10 +1180,11 @@ Public Class frmPS4Twitch
 
             Dim a As New asm
 
-            a.AddVar("hook", rpCtrlWrap + &H1BF670)
+            a.AddVar("hook", rpCtrlWrap + &H1BEB90)
             a.AddVar("newmem", hookmem)
             a.AddVar("newctrl", hookmem + &H400)
-            a.AddVar("hookreturn", rpCtrlWrap + &H1BF676)
+            'a.AddVar("hookreturn", rpCtrlWrap + &H1BF676)
+            a.AddVar("hookreturn", rpCtrlWrap + &H1BEB96)
 
             a.pos = hookmem
             a.Asm("mov edx, newctrl")
@@ -1198,10 +1199,10 @@ Public Class frmPS4Twitch
 
             a.Clear()
             a.AddVar("newmem", hookmem)
-            a.pos = rpCtrlWrap + &H1BF670
+            a.pos = rpCtrlWrap + &H1BEB90
             a.Asm("jmp newmem")
 
-            WriteProcessMemory(_targetProcessHandle, rpCtrlWrap + &H1BF670, a.bytes, a.bytes.Length, 0)
+            WriteProcessMemory(_targetProcessHandle, rpCtrlWrap + &H1BEB90, a.bytes, a.bytes.Length, 0)
 
 
 
@@ -1216,7 +1217,7 @@ Public Class frmPS4Twitch
     End Sub
     Private Sub RestoreControl()
         'WBytes(rpCtrlWrap + &H1D0980, {&H8B, &H55, &HC, &H83, &HC4, &HC})  'Old ver
-        WBytes(rpCtrlWrap + &H1BF670, {&H8B, &H55, &HC, &H83, &HC4, &HC})
+        WBytes(rpCtrlWrap + &H1BEB90, {&H8B, &H55, &HC, &H83, &HC4, &HC})
 
         'pressthread.Abort
     End Sub
