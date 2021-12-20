25ab9027393011d8e8f245b90488b956fdf82e0d
Wulf2k <wulf2k@gmail.com>
2021-12-20 00:23:53 -0600

    c

--- PS4-TwitchControl/Form1.vb
+++ PS4-TwitchControl/Form1.vb
@@ -66,16 +66,6 @@ Partial Public Class frmPS4Twitch
     End Sub
 
     Private Sub ProcessCMD(user As String, role As String, cmd As String)
-        'ProcessCMD__Switch(user, role, cmd)
-        'ProcessCMD__XB1(user, role, cmd)
-        'ProcessCMD_Celeste(user, role, cmd)
-        'ProcessCMD_DarkSoulsRemastered(user, role, cmd)
-        'ProcessCMD_PokemonFireRed(user, role, cmd)
-        'ProcessCMD_PokemonPlatinum(user, role, cmd)
-        'ProcessCMD_SilentHill2(user, role, cmd)
-        'ProcessCMD_SuperMeatBoy(user, role, cmd)
-        'ProcessCMD_ZeldaOOT(user, role, cmd)
-        'ProcessCMD_ZeldaTP(user, role, cmd)
 
 
         If user = "wulf2kbot" Then Return
@@ -84,6 +74,10 @@ Partial Public Class frmPS4Twitch
         Dim tmpuser = user
         Dim tmpcmd = cmd
         Dim CMDmulti As Integer = 1
+
+
+        tmpcmd = tmpcmd.ToLower
+
 
         If tmpcmd.Contains("wulf") Then
             Return
@@ -105,6 +99,26 @@ Partial Public Class frmPS4Twitch
             Next
             outputChat(txt)
             Return
+        End If
+
+        If tmpcmd = "#macrolist" Then
+            Dim txt As String = ""
+            For Each pair As KeyValuePair(Of String, String) In macros
+                txt += pair.Key + ", "
+            Next
+            outputChat(txt)
+            Return
+        End If
+
+        If tmpcmd.IndexOf("#macroshow") = 0 Then
+            For Each pair As KeyValuePair(Of String, String) In macros
+                If tmpcmd.Contains(" ") AndAlso pair.Key = tmpcmd.Split(" ")(1) Then
+                    outputChat(pair.Value)
+                    Return
+                End If
+                outputChat("Macro not found.")
+                Return
+            Next
         End If
 
         'If tmpcmd.IndexOf("#authadd") = 0 Then
@@ -162,7 +176,7 @@ Partial Public Class frmPS4Twitch
             Return
         End If
 
-        tmpcmd = tmpcmd.ToLower
+
         tmpcmd = tmpcmd.Replace(" ", "")
 
 
@@ -233,8 +247,17 @@ Partial Public Class frmPS4Twitch
                 execCMD(tmpuser, role, "h-1")
                 outputChat("All commands for " & tmpuser & " removed from queue.")
                 Return
-
         End Select
+
+
+        If tmpcmd.IndexOf("#") = 0 Then
+            For Each pair As KeyValuePair(Of String, String) In macros
+                If pair.Key = tmpcmd Then tmpcmd = pair.Value
+            Next
+            ProcessCMD(tmpuser, role, tmpcmd)
+            Return
+        End If
+
 
         For i = 0 To CMDmulti - 1
             execCMD(tmpuser, role, tmpcmd)
@@ -281,52 +304,13 @@ Partial Public Class frmPS4Twitch
         End Function
     End Structure
 
-    Private Declare Function CloseHandle Lib "kernel32.dll" (ByVal hObject As IntPtr) As Boolean
-    Private Declare Function CreateRemoteThread Lib "kernel32" (ByVal hProcess As Integer, ByVal lpThreadAttributes As Integer, ByVal dwStackSize As Integer, ByVal lpStartAddress As Integer, ByVal lpParameter As Integer, ByVal dwCreationFlags As Integer, ByRef lpThreadId As Integer) As Integer
-    Private Declare Function FindWindowA Lib "user32.dll" (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
-    Private Declare Function FindWindowExA Lib "user32.dll" (ByVal hWnd1 As Integer, ByVal hWnd2 As Integer, ByVal lpsz1 As String, ByVal lpsz2 As String) As Integer
-    Private Declare Function GetWindowRect Lib "user32" (ByVal hWnd As IntPtr, ByRef lpRect As RECT) As Long
-    Private Declare Function OpenProcess Lib "kernel32.dll" (ByVal dwDesiredAcess As UInt32, ByVal bInheritHandle As Boolean, ByVal dwProcessId As Int32) As IntPtr
-    Private Declare Function ReadProcessMemory Lib "kernel32" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Boolean
-    Private Declare Function SetForegroundWindow Lib "user32.dll" (ByVal hwnd As Integer) As Integer
-    Private Declare Function ShowWindow Lib "user32.dll" (ByVal hwnd As Integer, nCmdShow As Integer) As Integer
-    Private Declare Function VirtualAllocEx Lib "kernel32.dll" (ByVal hProcess As IntPtr, ByVal lpAddress As IntPtr, ByVal dwSize As IntPtr, ByVal flAllocationType As Integer, ByVal flProtect As Integer) As IntPtr
-    Private Declare Function VirtualProtectEx Lib "kernel32.dll" (hProcess As IntPtr, lpAddress As IntPtr, ByVal lpSize As IntPtr, ByVal dwNewProtect As UInt32, ByRef dwOldProtect As UInt32) As Boolean
-    Private Declare Function VirtualFreeEx Lib "kernel32.dll" (hProcess As IntPtr, lpAddress As IntPtr, ByVal dwSize As Integer, ByVal dwFreeType As Integer) As Boolean
-    Private Declare Function WriteProcessMemory Lib "kernel32" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByVal lpNumberOfBytesWritten As Integer) As Boolean
 
-
-
-
-    Public Const PROCESS_VM_READ = &H10
-    Public Const TH32CS_SNAPPROCESS = &H2
-    Public Const MEM_COMMIT = 4096
-    Public Const MEM_RELEASE = &H8000
-    Public Const PAGE_READWRITE = 4
-    Public Const PAGE_EXECUTE_READWRITE = &H40
-    Public Const PROCESS_CREATE_THREAD = (&H2)
-    Public Const PROCESS_VM_OPERATION = (&H8)
-    Public Const PROCESS_VM_WRITE = (&H20)
-    Public Const PROCESS_ALL_ACCESS = &H1F0FFF
-
-    Private _targetProcess As Process = Nothing 'to keep track of it. not used yet.
-    Private _targetProcessHandle As IntPtr = IntPtr.Zero 'Used for ReadProcessMemory
 
     Dim modlist As New List(Of String)
     Dim trilist As New List(Of String)
     Dim ignorelist As New List(Of String)
 
-
-    'RemotePlay app addresses
-    Private rpBase As IntPtr = 0
-    Private rpCtrlWrap As IntPtr = 0
-    Private wow64 As IntPtr = 0
-
-    'Our newly created memory
-    Dim hookmem As IntPtr
-
-    'Hook in RemotePlay's code
-    Dim hookloc As IntPtr
+    Dim macros As List(Of KeyValuePair(Of String, String)) = New List(Of KeyValuePair(Of String, String))
 
 
     'Button values
@@ -394,67 +378,7 @@ Partial Public Class frmPS4Twitch
 
 
 
-    Public Function ScanForProcess(ByVal procName As String, Optional automatic As Boolean = False) As Boolean
 
-        Dim _allProcesses() As Process = Process.GetProcesses
-        For Each pp As Process In _allProcesses
-            'If pp.MainWindowTitle.ToLower.Equals(procName.ToLower) Then
-            If pp.ProcessName = procName Then
-                'found it! proceed.
-                Return TryAttachToProcess(pp, automatic)
-            End If
-        Next
-        Return False
-    End Function
-    Public Function TryAttachToProcess(ByVal proc As Process, Optional automatic As Boolean = False) As Boolean
-        If Not (_targetProcessHandle = IntPtr.Zero) Then
-            DetachFromProcess()
-        End If
-
-        _targetProcess = proc
-        _targetProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, False, _targetProcess.Id)
-        If _targetProcessHandle = 0 Then
-            If Not automatic Then 'Showing 2 message boxes as soon as you start the program is too annoying.
-                MessageBox.Show("Failed to attach to process.")
-            End If
-
-            Return False
-        Else
-            'if we get here, all connected and ready to use ReadProcessMemory()
-
-            Return True
-            'MessageBox.Show("OpenProcess() OK")
-        End If
-
-    End Function
-    Public Sub DetachFromProcess()
-        If Not (_targetProcessHandle = IntPtr.Zero) Then
-            _targetProcess = Nothing
-            Try
-                CloseHandle(_targetProcessHandle)
-                _targetProcessHandle = IntPtr.Zero
-                'MessageBox.Show("MemReader::Detach() OK")
-            Catch ex As Exception
-                'MessageBox.Show("Warning: MemoryManager::DetachFromProcess::CloseHandle error " & Environment.NewLine & ex.Message)
-            End Try
-        End If
-    End Sub
-
-
-    Private Sub findDllAddresses()
-        For Each dll As ProcessModule In _targetProcess.Modules
-            Select Case dll.ModuleName.ToLower
-                Case "remoteplay.exe"
-                    rpBase = dll.BaseAddress
-
-                Case "rpctrlwrapper.dll"
-                    rpCtrlWrap = dll.BaseAddress
-
-                Case "wow64.dll"
-                    wow64 = dll.BaseAddress
-            End Select
-        Next
-    End Sub
 
     Private Sub frmPS4Twitch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -471,6 +395,10 @@ Partial Public Class frmPS4Twitch
         modlist.Add("wulf2k")
         modlist.Add("yuidesu")
 
+
+
+        macros.Add(New KeyValuePair(Of String, String)("#test1", "hx10,wb,wr,hx10"))
+        macros.Add(New KeyValuePair(Of String, String)("#test2", "hx5,wf,wl,hx5"))
 
 
     End Sub
@@ -622,144 +550,9 @@ Partial Public Class frmPS4Twitch
             'RestoreControl()
         End If
     End Sub
-
-
-    Public Function RInt8(ByVal addr As IntPtr) As SByte
-        Dim _rtnBytes(0) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 1, vbNull)
-        Return _rtnBytes(0)
-    End Function
-    Public Function RInt16(ByVal addr As IntPtr) As Int16
-        Dim _rtnBytes(1) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 2, vbNull)
-        Return BitConverter.ToInt16(_rtnBytes, 0)
-    End Function
-    Public Function RInt32(ByVal addr As IntPtr) As Int32
-        Dim _rtnBytes(3) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
-
-        Return BitConverter.ToInt32(_rtnBytes, 0)
-    End Function
-    Public Function RInt64(ByVal addr As IntPtr) As Int64
-        Dim _rtnBytes(7) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
-        Return BitConverter.ToInt64(_rtnBytes, 0)
-    End Function
-    Public Function RUInt16(ByVal addr As IntPtr) As UInt16
-        Dim _rtnBytes(1) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 2, vbNull)
-        Return BitConverter.ToUInt16(_rtnBytes, 0)
-    End Function
-    Public Function RUInt32(ByVal addr As IntPtr) As UInt32
-        Dim _rtnBytes(3) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
-        Return BitConverter.ToUInt32(_rtnBytes, 0)
-    End Function
-    Public Function RUInt64(ByVal addr As IntPtr) As UInt64
-        Dim _rtnBytes(7) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
-        Return BitConverter.ToUInt64(_rtnBytes, 0)
-    End Function
-    Public Function RSingle(ByVal addr As IntPtr) As Single
-        Dim _rtnBytes(3) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
-        Return BitConverter.ToSingle(_rtnBytes, 0)
-    End Function
-    Public Function RDouble(ByVal addr As IntPtr) As Double
-        Dim _rtnBytes(7) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
-        Return BitConverter.ToDouble(_rtnBytes, 0)
-    End Function
-    Public Function RIntPtr(ByVal addr As IntPtr) As IntPtr
-        Dim _rtnBytes(IntPtr.Size - 1) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, IntPtr.Size, Nothing)
-        If IntPtr.Size = 4 Then
-            Return New IntPtr(BitConverter.ToUInt32(_rtnBytes, 0))
-        Else
-            Return New IntPtr(BitConverter.ToInt64(_rtnBytes, 0))
-        End If
-    End Function
-    Public Function RBytes(ByVal addr As IntPtr, ByVal size As Int32) As Byte()
-        Dim _rtnBytes(size - 1) As Byte
-        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, size, vbNull)
-        Return _rtnBytes
-    End Function
-    Private Function RAscStr(ByVal addr As UInteger) As String
-        Dim Str As String = ""
-        Dim cont As Boolean = True
-        Dim loc As Integer = 0
-
-        Dim bytes(&H10) As Byte
-
-        ReadProcessMemory(_targetProcessHandle, addr, bytes, &H10, vbNull)
-
-        While (cont And loc < &H10)
-            If bytes(loc) > 0 Then
-
-                Str = Str + Convert.ToChar(bytes(loc))
-
-                loc += 1
-            Else
-                cont = False
-            End If
-        End While
-
-        Return Str
-    End Function
-    Private Function RUniStr(ByVal addr As UInteger) As String
-        Dim Str As String = ""
-        Dim cont As Boolean = True
-        Dim loc As Integer = 0
-
-        Dim bytes(&H20) As Byte
-
-
-        ReadProcessMemory(_targetProcessHandle, addr, bytes, &H20, vbNull)
-
-        While (cont And loc < &H20)
-            If bytes(loc) > 0 Then
-
-                Str = Str + Convert.ToChar(bytes(loc))
-
-                loc += 2
-            Else
-                cont = False
-            End If
-        End While
-
-        Return Str
-    End Function
-
-    Public Sub WInt8(ByVal addr As IntPtr, val As SByte)
-        WriteProcessMemory(_targetProcessHandle, addr, {val}, 1, Nothing)
-    End Sub
-    Public Sub WInt16(ByVal addr As IntPtr, val As Int16)
-        WriteProcessMemory(_targetProcessHandle, addr, BitConverter.GetBytes(val), 2, Nothing)
-    End Sub
-    Public Sub WInt32(ByVal addr As IntPtr, val As Int32)
-        WriteProcessMemory(_targetProcessHandle, addr, BitConverter.GetBytes(val), 4, Nothing)
-    End Sub
-    Public Sub WUInt8(ByVal addr As IntPtr, val As Byte)
-        WriteProcessMemory(_targetProcessHandle, addr, {val}, 1, Nothing)
-    End Sub
-    Public Sub WUInt16(ByVal addr As IntPtr, val As UInt16)
-        WriteProcessMemory(_targetProcessHandle, addr, BitConverter.GetBytes(val), 2, Nothing)
-    End Sub
-    Public Sub WUInt32(ByVal addr As IntPtr, val As UInt32)
-        WriteProcessMemory(_targetProcessHandle, addr, BitConverter.GetBytes(val), 4, Nothing)
-    End Sub
-    Public Sub WSingle(ByVal addr As IntPtr, val As Single)
-        WriteProcessMemory(_targetProcessHandle, addr, BitConverter.GetBytes(val), 4, Nothing)
-    End Sub
-    Public Sub WBytes(ByVal addr As IntPtr, val As Byte())
-        WriteProcessMemory(_targetProcessHandle, addr, val, val.Length, Nothing)
-    End Sub
-    Public Sub WAscStr(addr As UInteger, str As String)
-        WriteProcessMemory(_targetProcessHandle, addr, System.Text.Encoding.ASCII.GetBytes(str), str.Length, Nothing)
-    End Sub
-
-
 End Class
+
+
 
 
 Public Class QdInput
